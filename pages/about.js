a8576fb8f9292fe7e2ba26cf04529196d92079f6
苦しみ悶える <s23013@std.it-college.ac.jp>
2023-12-20 08:18:31 +0000

    chapter5-5

--- pages/about.js
+++ pages/about.js
@@ -2,12 +2,29 @@ import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
 import Contact from 'components/contact'
-import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar
+} from 'components/two-column'
+import Image from 'next/image'
+import eyecatch from 'next-react-website/blog-app-router/images/about.jpg'
 
 export default function About () {
   return (
     <Container>
       <Hero title='About' subtitle='About development activities' />
+
+      <figure>
+        <Image
+          src={eyecatch}
+          alt=''
+          layout='responsive'
+          sizes='(min-width: 1152px) 1152px, 100vw'
+          priority
+          placeholder='blur'
+        />
+      </figure>
 
       <TwoColumn>
         <TwoColumnMain>
