faad694fc6e41b0f9cdbaa9773ad609627bc56e1
苦しみ悶える <s23013@std.it-college.ac.jp>
2023-12-21 07:36:28 +0000

    chapter5-9終わり

--- components/contact.js
+++ components/contact.js
@@ -1,9 +1,11 @@
+import Social from 'components/social'
 import styles from 'styles/container.module.css'
 
 export default function Contact () {
   return (
     <div className={styles.stack}>
       <h3 className={styles.heading}>Contact</h3>
+      <Social />
       <address>cube@web.mail.address</address>
     </div>
   )
